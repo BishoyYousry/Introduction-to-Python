ea25ed581db8c4aa78d1a288ef12ecdf64155323
Bishoy Yousry <1900733@eng.asu.edu.eg>
2024-12-27 02:13:37 +0200

    Add Tut 1

---
+++ Tutorial 1/Module Assessments/Module Assesment 1/first.py
@@ -0,0 +1,4 @@
+info = '''My name is Bishoy Yousry, I'm 23 years old,
+in this course I want to learn using python in professional way, use it in automation, know 
+intro about ML, AL, Data analysis'''
+print(info)
