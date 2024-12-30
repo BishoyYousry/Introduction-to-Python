156621558464a6f7aa6c41f17e46bde4ee906682
Bishoy Yousry <1900733@eng.asu.edu.eg>
2024-12-30 02:34:11 +0200

    Modify the average

--- average/average.py
+++ average/average.py
@@ -1,7 +1,7 @@
-def average(list):
-    sum = sum(list)
-    avg = sum / len(list)
+def average(lst):
+    total = sum(lst) 
+    avg = total / len(lst)
     return avg
 
-list = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
-average()
+lst = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+print(average(lst))
